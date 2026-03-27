5cd5a807d4118144d23ba7bc0b16b1e889dc8f5f
takuya_kiyoshima <takuya.kiyoshima@techshare.co.jp>
2026-03-27 14:18:19 +0800

    ignore build/install/log

--- ros2_ws/build/limo_base/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/root/ros2_ws/install/limo_msgs/share/limo_msgs/package.sh"
--- ros2_ws/build/limo_bringup/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /root/ros2_ws/src/limo_ros2/limo_bringup
-# Build directory: /root/ros2_ws/build/limo_bringup
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
